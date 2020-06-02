855ce6dd84c01ed5ffd0e4d20f73c1cca4a715ed
Pierre Garcia <pg.pierre.garcia@free.fr>
2020-06-02 19:32:30 +0200

    fix empty validates on skill model

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,5 +1,4 @@
 class Skill < ApplicationRecord
   has_one_attached :icon
   validates :text, presence: true, length: { maximum: 16, too_long: '%{count} characters is the maximum allowed' }
-  validates :iconName
 end
