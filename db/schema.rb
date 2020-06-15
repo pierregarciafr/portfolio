d5ce85341a2722c1e5df0a72f2cfb40162d38488
Pierre Garcia <pg.pierre.garcia@free.fr>
2020-06-15 13:51:08 +0200

    change icon name in the model to fit remote

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_03_000127) do
+ActiveRecord::Schema.define(version: 2020_06_15_114326) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,7 +39,7 @@ ActiveRecord::Schema.define(version: 2020_06_03_000127) do
   create_table "bios", force: :cascade do |t|
     t.string "title"
     t.string "content"
-    t.string "iconLink"
+    t.string "icon"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
