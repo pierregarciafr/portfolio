91d00ecf3c3f58be50a54b300b5a73ee8a361d24
Pierre Garcia <pg.pierre.garcia@free.fr>
2020-07-08 15:28:43 +0200

    adapt after uploading remote pg db

--- db/migrate/20200615114326_rename_column_iconlink_to_icon_in_bio.rb
+++
@@ -1,5 +0,0 @@
-class RenameColumnIconlinkToIconInBio < ActiveRecord::Migration[6.0]
-  def change
-    rename_column :bios, :iconLink, :icon
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_15_114326) do
+ActiveRecord::Schema.define(version: 2020_06_02_234150) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,15 +42,6 @@ ActiveRecord::Schema.define(version: 2020_06_15_114326) do
     t.string "icon"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "messages", force: :cascade do |t|
-    t.string "from"
-    t.string "object"
-    t.string "content"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.string "name"
   end
 
   create_table "projects", force: :cascade do |t|
