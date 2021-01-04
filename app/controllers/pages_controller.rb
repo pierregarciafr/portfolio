e6de5e855c34ecfe4817229a39f9d55a84c8ccde
pierre garcia <pg.pierre.garcia@free.fr>
2021-01-04 01:04:48 +0100

    change var name from 'portfolio' to 'dev_project(s)' where necessary

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
 
   def home
     @skills = Skill.all.sort { |a, b| a.id <=> b.id }
-    @portfolio = Project.all.sort { |a, b| a.id <=> b.id }
+    @dev_projects = Project.all.sort { |a, b| a.id <=> b.id }
     @bios = Bio.all
   end
 
